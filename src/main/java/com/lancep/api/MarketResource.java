84b4c3ebbc44f2a5d0eac05926f1c2ece8c5a39a
Lance Poehler <lancepoehler@gmail.com>
2016-11-15 01:01:36 -0800

    Added in a parameter

--- src/main/java/com/lancep/api/MarketResource.java
+++ src/main/java/com/lancep/api/MarketResource.java
@@ -1,15 +1,22 @@
 package com.lancep.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.HttpURLConnection;
 
 @Path("/checkout")
 public class MarketResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getMessage() {
-        return "Hello world!";
+    public String getBill(@QueryParam("productList") String productList) {
+        if (productList == null) {
+            throw new WebApplicationException(
+                Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+                        .entity("name parameter is mandatory")
+                        .build()
+            );
+        }
+        return "Hello world!" + productList;
     }
 }
