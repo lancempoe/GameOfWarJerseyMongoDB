e496d57ffecd59e1fdc2a99d4cfffb4dc0f93ee5
Lance Poehler <lancepoehler@gmail.com>
2016-11-15 23:21:10 -0800

    Removing comment

--- src/main/java/com/lancep/RestApplication.java
+++ src/main/java/com/lancep/RestApplication.java
@@ -4,9 +4,7 @@ import com.lancep.api.MarketResource;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class RestApplication extends ResourceConfig {
-    /**
-     * Register JAX-RS application components.
-     */
+
     public RestApplication() {
         // register application resources
         register(MarketResource.class);
