858fdfec6ca1bbe9fee533e820e6c198132ada03
Lance Poehler <lancepoehler@gmail.com>
2016-11-16 22:13:33 -0800

    Built a streaming csv file for download. This shows how to download a very large file as it is being built.

--- src/main/java/com/lancep/RestApplication.java
+++ src/main/java/com/lancep/RestApplication.java
@@ -1,5 +1,6 @@
 package com.lancep;
 
+import com.lancep.api.CSVResource;
 import com.lancep.api.MarketResource;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -8,5 +9,6 @@ public class RestApplication extends ResourceConfig {
     public RestApplication() {
         // register application resources
         register(MarketResource.class);
+        register(CSVResource.class);
     }
 }
