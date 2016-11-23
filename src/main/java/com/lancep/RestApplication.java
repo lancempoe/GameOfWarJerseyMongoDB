6a26f727f516567cd3760b26c409b891f47c2c4e
Lance Poehler <lancepoehler@gmail.com>
2016-11-22 23:30:33 -0800

    fixing droped class

--- src/main/java/com/lancep/RestApplication.java
+++ src/main/java/com/lancep/RestApplication.java
@@ -1,6 +1,5 @@
 package com.lancep;
 
-import com.lancep.api.CSVResource;
 import com.lancep.api.MarketResource;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -9,6 +8,5 @@ public class RestApplication extends ResourceConfig {
     public RestApplication() {
         // register application resources
         register(MarketResource.class);
-        register(CSVResource.class);
     }
 }
